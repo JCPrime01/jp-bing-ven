89b01a9fac7e432e07bcd62b87b40f0fcd77d2f7
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 21:18:23 -0300

    chore: update shareCode to ATW41NTYJ8M

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=N3L1HO3OGOR&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=ATW41NTYJ8M&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
