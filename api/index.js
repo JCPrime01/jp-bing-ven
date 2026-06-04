69a94643c19bd30dd18a37d82db0999d20d168bb
JCPrime01 <vanzokiller@gmail.com>
2026-06-04 10:06:23 -0300

    chore: shareCode P23N05R3W8Q -> LF56FNDMKJG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=P23N05R3W8Q&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=LF56FNDMKJG&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
